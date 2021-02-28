9341306b6cb0280d0d3e7449cc8a8287d87891ef
z-o-rn <baseball9tapocar@icloud.com>
2021-02-28 12:54:16 +0000

    db:migrate reset

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2021_02_24_003408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.text "introduction"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -46,7 +45,6 @@ ActiveRecord::Schema.define(version: 2021_02_24_003408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
